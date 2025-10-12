82f122523c4db9e552f535581cff049481f35b04
Marcos Paulo <marcospaulosmoreiramp11@gmail.com>
2025-10-12 13:00:04 -0300

    Adicionado a pasta 'games' e readicionado o primeiro jogo dos dojos

---
+++ games/game-a/game-a.c
@@ -0,0 +1,77 @@
+// Jogo: Encontre o número aleatório
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    srand(time(NULL));
+
+    // linha e coluna aleatória
+    int ln, cl;
+
+    // numeros aleatórios
+    int pd, al;
+
+    // escolha e acumulador
+    int esc, cont = 0;
+
+    // números de linhas e colunas
+    int numLine = 10, numCol = 25;
+
+    for (int n = 0; n < 10; n++) {
+        do {
+            pd = 1 + rand() % 9;
+            al = 1 + rand() % 9;
+        } while (pd == al);
+
+        ln = rand() % numLine;
+        cl = rand() % numCol;
+
+        // geração da tabéla
+        for (int l = 0; l < numLine; l++) {
+            // geração dos elementos das linhas da tabéla
+            for (int c = 0; c < numCol; c++) {
+                // implementação do número diferente
+                if (ln == l && cl == c) {
+                    printf("%d ", al);
+                    continue;
+                }
+
+                // implementação do número padrão
+                printf("%d ", pd);
+            }
+            printf("\n");
+        }
+
+        system("sleep 3");
+        system("clear");
+
+        printf("Qual era o número diferente: ");
+        scanf("%d", &esc);
+
+        if (al == esc) {
+            cont++;
+            printf("Você acertou! :D\n\n");
+        } else {
+            printf("Chute para fora! :C\n\n");
+        }
+
+        // recurso para limpar o buffer
+        while (getchar() != '\n') {};
+
+        printf("Presione ENTER para continuar...\n");
+
+        // setbuf(stdin,NULL);
+
+        getchar();
+        system("clear");
+
+        // crescimento dinâmico da tabéla ao passar de tentativa
+        numCol += 2;
+        numLine += 2;
+    }
+
+    printf("Acertou %i/10\n", cont);
+    return 0;
+}
